443e2c25171f0e9f5eeee9d5312ab390af4855fa
Zeyad88Gowaily <zeyad47.ahmed@gmail.com>
2024-12-23 22:45:56 +0200

    Update AdminHomePageApp.java

--- java/com/example/myjavafx/AdminHomePageApp.java
+++ java/com/example/myjavafx/AdminHomePageApp.java
@@ -12,11 +12,9 @@ public class AdminHomePageApp extends Application {
     @Override
     public void start(Stage primaryStage) {
         try {
-            // Load the Admin Home Page FXML
             FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/myjavafx/AdminHomePage.fxml"));
             Scene scene = new Scene(fxmlLoader.load());
 
-            // Set up the primary stage
             primaryStage.setTitle("Admin Control Center");
             primaryStage.setScene(scene);
             primaryStage.show();
