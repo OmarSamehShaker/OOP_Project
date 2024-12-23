973d4628d561a64babbf2ca258bdca16a701c285
Zeyad88Gowaily <zeyad47.ahmed@gmail.com>
2024-12-23 22:52:03 +0200

    Update AdminViewProfileApp.java

--- java/com/example/myjavafx/AdminViewProfileApp.java
+++ java/com/example/myjavafx/AdminViewProfileApp.java
@@ -10,11 +10,9 @@ public class AdminViewProfileApp extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        // Load the FXML for Admin View Profile page
         FXMLLoader loader = new FXMLLoader(getClass().getResource("Admin_View_Profile.fxml"));
-        VBox root = loader.load(); // Changed from AnchorPane to VBox
-
-        // Set up the scene and the stage
+        VBox root = loader.load(); 
+        
         Scene scene = new Scene(root);
         primaryStage.setScene(scene);
         primaryStage.setTitle("Admin View Profile");
